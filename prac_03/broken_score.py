614a198b58826f818f2ff0023c377c8b525e1cf5
admin <akidulatini@mail.ru>
2020-08-05 14:02:31 +0600

    Refactor broken score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -6,18 +6,21 @@ Program to determine score status
 
 def main():
     score = int(input("Enter score: "))
+    print(get_score(score))
 
+
+def get_score(score):
     if score < 0 or score > 100:
-        print("Invalid")
+        return "Invalid"
 
     elif score >= 90:
-        print("Excellent")
+        return "Excellent"
 
     elif score >= 50:
-        print("Pass")
+        return "Pass"
 
     elif score < 50:
-        print("Bad")
+        return "Bad"
 
 
 if __name__ == '__main__':
