474113e6b117ef9a924606ad848b0b9a306cf060
admin <akidulatini@mail.ru>
2020-08-06 11:50:02 +0600

    Submit it

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -29,19 +29,16 @@ def main():
     a_names = [name for name in names if name.startswith('A')]
     print(a_names)
 
-    # TODO: use a list comprehension to create a list of all of the full_names
-    # in lowercase format
+    # list of all of the full_names in lowercase format
     lowercase_full_names = [name.lower() for name in full_names]
     print(lowercase_full_names)
 
     almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-    # TODO: use a list comprehension to create a list of integers
-    # from the above list of strings
+    # list of integers from the above list of strings
     numbers = [int(number) for number in almost_numbers]
     print(numbers)
 
-    # TODO: use a list comprehension to create a list of only the numbers that are
-    # greater than 9 from the numbers (not strings) you just created
+    # list of only the numbers that are greater than 9 from the numbers
     double_figures = [number for number in numbers if number > 9]
     print(double_figures)
 
